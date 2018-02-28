21af06ed560075464e86afa4e468973d3530be7e
VaibhavP189 <34150991+VaibhavP189@users.noreply.github.com>
2018-02-28 22:34:59 +0530

    Add files via upload

---
+++ ExceedLimitsPuzzle.java
@@ -0,0 +1,62 @@
+package exceedlimit;
+
+public class ExceedLimitsPuzzle {
+	public static void main(String[] args) {
+		short counter = 1;
+		float sum = 0;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "  " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "  " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "  " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "  " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "  " + sum);
+
+}
+}
