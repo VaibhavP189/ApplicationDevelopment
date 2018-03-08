ec7ac50c2dbb398e56138d2063f9b77a0edde6df
VaibhavP189 <34150991+VaibhavP189@users.noreply.github.com>
2018-03-08 20:50:03 +0530

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,12 @@
 package CALCULATOR;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 
 public class Calculator {
-    public static void main(String[] args) {
+
+
+	public static void main(String[] args) {
         @SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
         System.out.println("Enter your first integer value: ");
@@ -17,18 +20,27 @@ public class Calculator {
 
         System.out.println("Enter your second error term value: ");
         double seconderror = input.nextDouble();
+
+        double mpy = first * second;
+        double div = first / second;
+
+        double errorterm1 = Math.abs((firsterror / first + seconderror / second) * mpy);
+        double errorterm2 = (firsterror / first + seconderror / second) * div;
         
-        double plus = first + second;
-        double minus = first - second;
-        double errorterm = firsterror + seconderror;
+        String errortermdiv = String.valueOf(Math.abs(errorterm2));
+        DecimalFormat roundoffMpy = new DecimalFormat("####.##");
+        String roundedMpy = roundoffMpy.format(errorterm1);
+        
+        
+     
+        System.out.println("Multiplication for " + first + " and " + second
+                + " is: \t\t" + mpy);
+        System.out.println("Error Term for Multiplication of" + first + "and" + second + "is:\t\t" + roundedMpy);
+        
+        System.out.println("Division for " + first + " and " + second
+                + " is: \t\t" + div);
+        System.out.println("Error Term for Division of" + first + "and" + second + "is:\t\t" + errortermdiv);   
 
 
-        System.out.println("Summation for " + first + " and " + second
-                + " is: \t\t" + plus);
-        System.out.println("Error Term for submmation of" + first + "and" + second + "is:\t\t" + errorterm);
-        
-        System.out.println("Subtraction for " + first + " and " + second
-                + " is: \t\t" + minus);
-        System.out.println("Error Term for subtraction of" + first + "and" + second + "is:\t\t" + errorterm);
     }
 }
