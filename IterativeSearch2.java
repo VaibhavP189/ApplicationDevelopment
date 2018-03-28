9615e2b42faf3144d450ede07ed6d2165ca7a4c1
VaibhavP189 <34150991+VaibhavP189@users.noreply.github.com>
2018-03-28 22:45:12 +0530

    Add files via upload

---
+++ IterativeSearch2.java
@@ -0,0 +1,24 @@
+package hello;
+
+public class IterativeSearch2 {
+	private static String[] elements = {"Vaibhav","Vaibhav M.","Jaskirat","Prince","Sukrut","Sumit S."};
+	
+	public static int iterativeSearch (String[] elements2, String goal) {
+		int ndx = 0;
+		while (ndx< elements.length && elements[ndx].equals(goal)) ndx++;
+		if (ndx >= elements.length) return -1;
+		if (goal.matches(elements[ndx])) return ndx;
+		else return -1;
+	}
+
+	public static void main(String[] args) {
+		String goal = "Sukrut";
+		int result = iterativeSearch(elements,goal);
+		if (result<0)
+			System.out.println("The goal could not find:" +""+ goal);
+		else
+			System.out.println("The goal of" + ""+goal +""+ "was found at index:" + result);
+	}
+
+	}
+
